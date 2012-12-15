a4c5579d47b12ea24376c98fc632bb74873e6566
Martin Schneider <info@outsmartin.de>
2012-12-15 16:47:39 +0100

    slideshows und bugfixes

--- presentation/source/javascripts/all.js
+++ presentation/source/javascripts/all.js
@@ -3,15 +3,33 @@ Reveal.initialize({
   progress: true,
   history: true,
   rollingLinks: false,
-  center: false,
+  center: true,
 
-  theme: Reveal.getQueryHash().theme || 'default', // available themehemes are in /css/theme
-  transition: Reveal.getQueryHash().transition || 'concave', // default/cube/page/concave/linear(2d)
+  theme: 'default', // available themehemes are in /css/theme
+  transition: 'default', // default/cube/page/concave/linear(2d)
+  dependencies: [
+        // Cross-browser shim that fully implements classList - https://github.com/eligrey/classList.js/
+        { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } }
+        //,
+        // Zoom in and out with Alt+click
+        //{ src: 'plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+
+        // Remote control your reveal.js presentation using a touch device
+        //{ src: 'plugin/remotes/remotes.js', async: true, condition: function() { return !!document.body.classList; } }
+    ]
 
   // Optional libraries used to extend on reveal.js
-  dependencies: [
-    { src: 'javascripts/classList.js', condition: function() { return !document.body.classList; } },
-    { src: 'javascripts/notes.js', async: true, condition: function() { return !!document.body.classList; } }
-
-  ]
 });
+$(document).ready(function() {
+  $('.imageslides').slides({
+    generateNextPrev: true,
+    effect: 'fade',
+    fadeEasing: "jswing",
+    crossfade: true,
+    fadeSpeed: 1000
+  });
+  $('.prev').text("<<");
+  $('.next').text(">>");
+});
+
+
